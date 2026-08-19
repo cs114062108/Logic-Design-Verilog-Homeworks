8cab35eae66bdfd9e0b1a915d097fa0dee2b68f5
cs114062108 <cs114062108@gapp.nthu.edu.tw>
2026-08-19 08:09:04 +0800

    HW3 mealy machine | pattern file load test

--- HW3/HW3_114062108_mod.v
+++ HW3/HW3_114062108_mod.v
@@ -5,11 +5,117 @@ module Warn_Pattern (
     input  wire in,
     output reg  [1:0] out
 );
+    // ---------------------------------
+    // FSM State Definitons
+    // ---------------------------------
+    localparam IDLE = 3'b000; // IDLE State
+    localparam S1   = 3'b001; // Received pattern: 1
+    localparam S2   = 3'b010; // Received pattern: 10  or 0110
+    localparam S3   = 3'b011; // Received pattern: 101
+    localparam S4   = 3'b100; // Received pattern: 0
+    localparam S5   = 3'b101; // Received pattern: 01
+    localparam S6   = 3'b110; // Received pattern: 011 or 1011
 
-    // TODO: state encoding
+    reg [2:0] state;
+    reg [2:0] next_state;
 
+    reg [1:0] next_out;
 
     // TODO: write more code
+    // --------------------------------------
+    // FSM State Register (Sequential)
+    // --------------------------------------
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            state <= IDLE;
+            out <= 2'b11;
+        end else begin
+            state <= next_state;
+            out <= next_out;
+        end
+    end
 
+    // TODO: state encoding
+    // ----------------------------------------------------------
+    // FSM Next State Logic & Output Assignments (Combinational)
+    // ----------------------------------------------------------
+    always @(*) begin
+        case (state)
+            IDLE: begin
+                if (in == 1'b0) begin
+                    next_state = S4;
+                    next_out = 2'b00;
+                end else begin
+                    next_state = S1;
+                    next_out = 2'b00;
+                end
+            end
+
+            S1: begin
+                if (in == 1'b0) begin
+                    next_state = S2;
+                    next_out = 2'b00;
+                end else begin
+                    next_state = S1;
+                    next_out = 2'b00;
+                end
+            end
+            
+            S2: begin
+                if (in == 1'b0) begin
+                    next_state = S4;
+                    next_out = 2'b00;
+                end else begin
+                    next_state = S3;
+                    next_out = 2'b00;
+                end
+            end
+
+            S3: begin
+                if (in == 1'b0) begin
+                    next_state = S2;
+                    next_out = 2'b00;
+                end else begin
+                    next_state = S6;
+                    next_out = 2'b01;
+                end
+            end
+
+            S4: begin
+                if (in == 1'b0) begin
+                    next_state = S4;
+                    next_out = 2'b00;
+                end else begin
+                    next_state = S5;
+                    next_out = 2'b00;
+                end
+            end
+
+            S5: begin
+                if (in == 1'b0) begin
+                    next_state = S2;
+                    next_out = 2'b00;
+                end else begin
+                    next_state = S6;
+                    next_out = 2'b00;
+                end
+            end
+
+            S6: begin
+                if (in == 1'b0) begin
+                    next_state = S2;
+                    next_out = 2'b01;
+                end else begin
+                    next_state = S1;
+                    next_out = 2'b00;
+                end
+            end
+
+            default: begin
+                next_state = IDLE;
+                next_out = 2'b00;
+            end
+        endcase
+    end
 
 endmodule
--- HW3/HW3_114062108_tb0.v
+++ HW3/HW3_114062108_tb0.v
@@ -8,6 +8,7 @@ module Warn_Pattern_tb0;
 
     // specify duration of a clock cycle.
     parameter cyc = 10;
+    parameter c = 5;
 
     // generate clock.
     always #(cyc/2) clk = ~clk;
@@ -22,14 +23,97 @@ module Warn_Pattern_tb0;
 
     // this blocks is for creating the waveform
     initial begin
-        $dumpfile("HW3_result.vcd");
+        $dumpfile("waves/HW3_result.vcd");
         $dumpvars(0, Warn_Pattern_tb0);
     end
 
     // do the testpatterns here
+    // Variables for file loading
+    integer file;
+    integer r_cnt, r_seq;
+    reg [3:0] A, B, C, D;
+    reg [15:0] buff_cnt;
+    
+    // Load the pattern file
+    initial begin
+        file = $fopen("./HW3/HW3_pat0.dat", "r");
+        r_cnt = $fscanf(file, "%b", buff_cnt);
+        {A, B, C, D} = buff_cnt;
+        $display("----------------------------------------------");
+        $display("              File Load Test");
+        $display("----------------------------------------------");
+        $display("A = %04b, B = %04b, C = %04b, D = %04b", A, B, C, D);
+        $display("A = %d, B = %d, C = %d, D = %d", A, B, C, D);
+        $display("----------------------------------------------");
+        $display("\n");
+        $fclose(file);
+    end
+    
     // the testpattern designs 
     initial begin
         //..write more code here ...//
+        $display("----------------------------------------------");
+        $display("Time\t CLK\t rst_n\t in\t | \t out ");
+        $display("----------------------------------------------");
+        $monitor("%0dns\t %b\t %b\t %b\t | \t %02b ", $time, clk, rst_n, in, out);
+
+        rst_n <= 1;
+        in <= 0;
+
+        @(negedge clk);
+        rst_n <= 1;
+
+        @(negedge clk);
+        rst_n <= 0;
+
+        @(negedge clk);
+        rst_n <= 1;
+        in <= 1;
+
+        @(negedge clk);
+        in <= 0;
+
+        @(negedge clk);
+        in <= 1;
+
+        @(negedge clk);
+        in <= 1;
+
+        @(negedge clk);
+        in <= 0;
+
+        @(negedge clk);
+        @(negedge clk);
+
+        @(negedge clk);
+        in <= 0;
+
+        @(negedge clk);
+        in <= 1;
+
+        @(negedge clk);
+        in <= 1;
+
+        @(negedge clk);
+        in <= 0;
+
+        @(negedge clk);
+        in <= 1;
+
+        @(negedge clk);
+        in <= 1;
+
+        @(negedge clk);
+        rst_n <= 0;
+        in <= 0;
+
+        @(negedge clk);
+        rst_n <= 1;
+
+        @(negedge clk);
+        @(negedge clk);
+        $display("----------------------------------------------");
+        $finish();
     end
 
 endmodule
